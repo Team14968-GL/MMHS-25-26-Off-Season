15a705f63e6ffe40d7478f0d26d1b88d26b7f56e
GL <386831@guhsd.net>
2026-03-03 13:37:18 -0800

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -18,10 +18,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(10.8862) //mass in Kilos
-            .forwardZeroPowerAcceleration(-44.792129192214446) //deceleration on the forward axis
-            .lateralZeroPowerAcceleration(-93.72450685019226) //deceleration on the lateral axis
+            .forwardZeroPowerAcceleration(-44.792129192214446) //Deceleration on the forward axis
+            .lateralZeroPowerAcceleration(-93.72450685019226) //Deceleration on the lateral axis
 
-            .translationalPIDFCoefficients(
+            .translationalPIDFCoefficients( //Adjusts how the robot moves side to side to correct it y-error
                     new PIDFCoefficients(
                             0.1,
                             0,
@@ -29,7 +29,7 @@ public class Constants {
                             0.025
                     )
             )
-            .headingPIDFCoefficients(
+            .headingPIDFCoefficients( //Adjusts how the robot turns to correct it's heading error
                     new PIDFCoefficients(
                             1.5,
                             0,
@@ -37,7 +37,7 @@ public class Constants {
                             0.01
                     )
             )
-            .drivePIDFCoefficients(
+            .drivePIDFCoefficients( //Adjusts how the robot moves forwards and backwards to correct it's x-error? more testing needed to confirm
                     new FilteredPIDFCoefficients(
                             0.1,
                             0.0,
@@ -46,31 +46,33 @@ public class Constants {
                             0.0
                     )
             )
-            
+            .centripetalScaling(0.005) //Adjusts how the robot follows curves
             ;
 
     public static MecanumConstants driveConstants = new MecanumConstants()
+            //Sets robot's max power
             .maxPower(1)
+            //Sets up motors in Pedro Pathing hardware map
             .rightFrontMotorName("rightFront")
             .rightRearMotorName("rightBack")
             .leftRearMotorName("leftBack")
             .leftFrontMotorName("leftFront")
-
+            //Sets up motor directions
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-
+            //Sets the robot's max velocity on the x and y axis
             .xVelocity(79.59548157218873) //Velocity on the forward axis
             .yVelocity(52.49261234313485); //Velocity on the lateral axis
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(0.625000528) //inches from center of rotation
+            .forwardPodY(0.625000528) //Inches from center of rotation on it's respective axis
             .strafePodX(5.1349999548) //^^^
-            .distanceUnit(DistanceUnit.INCH)
-            .hardwareMapName("pinpoint")
-            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD) //set accordingly
+            .distanceUnit(DistanceUnit.INCH) //Unit for measuring distance
+            .hardwareMapName("pinpoint") //Hardware map setup for Pedro Pathing localizer
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD) //Sets the encoder resolution to a preset for GoBilda Pinpoint
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD) //Direction that the encoder records data in
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED); //^^^
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
